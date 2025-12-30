e1b165b261b2ca8ab5bc3bb153c586dc1b5e9ebe
Irene0450 <irene.pesquera2021@gmail.com>
2025-12-30 21:10:36 +0100

    Update MyDialogueManager.gd

--- MyDialogueManager.gd
+++ MyDialogueManager.gd
@@ -9,9 +9,3 @@ func show_dialogue(title: String, local_resource: DialogueResource = null, extra
 		balloon.dialogue_line = dialogue_line
 		get_tree().current_scene.add_child(balloon)
 		show_dialogue(yield(balloon, "actioned"), local_resource, extra_game_states)
-
-#Para añadir un nuevo diálogo tienes que situarte en la pestaña de 2D, hacer click en el NPC y a la derecha,
-#en Script Variables, adjuntar el archivo dialogue.tres y poner el nombre del diálogo (en la pestaña
-#Dialogue, lo que empieza por ~)
-
-#NOTA: antes de hacer eso, asegúrate de que tiene un script con el código de 'Cama roja'
