8e570ee5df46caa8f6d874adec6ee6eead1448b7
Irene0450 <irene.pesquera2021@gmail.com>
2025-12-31 02:14:59 +0100

    Update and rename MyDialogueManager.gd to scripts/my_dialogue_manager.gd

--- scripts/my_dialogue_manager.gd
+++ scripts/my_dialogue_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+# Necesita el add-on de Nathan Hoad
+
 func show_dialogue(title: String, local_resource: DialogueResource = null, extra_game_states: Array = []) -> void:
 	get_tree().paused = true
 	var dialogue_line = yield(DialogueManager.get_next_dialogue_line(title, local_resource, extra_game_states), "completed")
