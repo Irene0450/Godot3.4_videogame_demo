d4342ff5d37c7cd0591ce760a0f1e9a2581d59d7
Irene0450 <irene.pesquera2021@gmail.com>
2025-12-31 02:20:15 +0100

    Update and rename SceneManager.gd to scripts/scene_manager.gd

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,42 +1,43 @@
 extends CanvasLayer
 
+# Gestiona la transición entre escenas
+
+# Variables
 var current_scene = ""
 var player_spawn_position = null
 
+# Gestión per se
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "Fade out":
-		#redirigir a escena tras fade
-		if current_scene == "Cabaña inicial":
-			get_tree().change_scene("res://Scenes/Inicio.tscn")
-		elif current_scene == "Entrar_Casa_Esha":
-			get_tree().change_scene("res://Scenes/CasaEsha.tscn")
-		elif current_scene == "CasaEsha":
-			get_tree().change_scene("res://Scenes/Inicio.tscn") # Falta configurar position
+		# Redirigir a escena tras animación fade
+		if current_scene == "cabaña_inicial":
+			get_tree().change_scene("res://scenes/inicio.tscn")
 		elif current_scene == "a_Inicio2":
-			get_tree().change_scene("res://Scenes/Inicio2.tscn")
-		# terminar animación
+			get_tree().change_scene("res://scenes/inicio2.tscn")
+		# Terminar animación
 		$AnimationPlayer.play("Fade in")
 	elif anim_name == "Fade in":
 		$TransitionColorFade.visible = false
 		
-		
 	elif anim_name == "Skull":
-		#redirigir a escena tras skull
-		if current_scene == "Inicio":
-			get_tree().change_scene("res://Combates/Zona batalla abad.tscn")
-		elif current_scene == "BatallaAbad":
+		# Redirigir a escena tras animación skull
+		if current_scene == "inicio":
+			get_tree().change_scene("res://combates/zona_batalla_abad.tscn")
+		elif current_scene == "batalla_abad":
 			get_tree().reload_current_scene()
 			StatsPlayer.health = 20
-		# terminar animación
+		# Terminar animación
 		$AnimationPlayer.play("Skull2")
 	elif anim_name == "Skull2":
 		$Skull_holder/Skull.visible = false
 		$TransitionColorSkull.visible = false
 
+# Animación de desvanecimiento / fundido en negro
 func fade_transition():
 	$TransitionColorFade.visible = true
 	$AnimationPlayer.play("Fade out")
 
+# Animación con calavera (combates)
 func skull_transition():
 	$TransitionColorSkull.visible = true
 	$Skull_holder/Skull.visible = true
