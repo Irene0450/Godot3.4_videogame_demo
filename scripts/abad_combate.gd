d7830387735beceacbb1c6862d12f477223171d7
Irene0450 <irene.pesquera2021@gmail.com>
2026-01-11 17:16:27 +0100

    Update and rename Abad combate.gd to abad_combate.gd

--- scripts/abad_combate.gd
+++ scripts/abad_combate.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
+# Variables
 var health = 16
-var damage = randi() % 2 + 1
+var damage = randi() % 2 + 1 # random
 var max_health = 16
 var damage_received = 0
 var attack_is_strong = false
 
+# Señales
 signal attack_made
 
+# Funciones
 func _ready():
 	$Node2D.visible = false
 	$ProgressBar.value = health * 100 / max_health
@@ -18,18 +21,16 @@ func _process(delta):
 
 func damage():
 	$Node2D.visible = true
-	$Node2D/Label.text = str(damage_received)
+	$Node2D/Label.text = str(damage_received) # Etiqueta con el valor del daño
 	$AnimationPlayer.play("Damage")
-	$ProgressBar.value = health * 100 / max_health
-
-
+	$ProgressBar.value = health * 100 / max_health # Actualizar barra salud
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if health <= 0:
 		if anim_name == "Damage":
 			$AnimationPlayer.play("Death")
 		elif anim_name == "Death":
-			SceneManager.current_scene = "a_Inicio2"
+			SceneManager.current_scene = "a_Inicio2" # Redirigir
 			SceneManager.fade_transition()
 	else:
 		if anim_name == "Damage":
